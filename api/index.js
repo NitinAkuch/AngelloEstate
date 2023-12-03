86bf2ca777b4160924f95aea84e17477530e6210
NitinAkuch <110688610+NitinAkuch@users.noreply.github.com>
2023-12-03 21:33:58 +0530

    Create Pages with routes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  console.log("GET method");
+});
 
 app.listen(3000, () => {
   console.log("listening on 3000...!");
