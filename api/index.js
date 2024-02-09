8d6e6afe1b79d0564c28f3e8b0f02f522aaaf0cc
NitinAkuch <110688610+NitinAkuch@users.noreply.github.com>
2024-02-09 11:14:02 +0530

    test github

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ mongoose
 app.use(express.json());
 
 app.listen(3000, () => {
-  console.log("listening on 3000...!");
+  console.log("listening on 3000...!!!");
 });
 
 app.use("/api/user", userRouter);
