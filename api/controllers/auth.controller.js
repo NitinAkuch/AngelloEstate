8e54e8e534bfc18e7b63f5a18e314da3690c6938
NitinAkuch <110688610+NitinAkuch@users.noreply.github.com>
2024-02-12 15:04:17 +0530

    Complete Signup, signin functionality and signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,3 +15,30 @@ export const signUp = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    console.log("Checking email");
+    const validUser = await User.findOne({ email: email });
+
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_KEY);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+    console.log("Sign in successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+// if (validUser) {
+//   console.log("User Found" + " " + validUser.username);
+// }
