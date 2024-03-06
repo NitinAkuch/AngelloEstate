d56bec9f3dbf6b8f65205a396da69d99c0a4c295
NitinAkuch <110688610+NitinAkuch@users.noreply.github.com>
2024-03-06 07:25:53 +0530

    Tailwind Line Clamp Configure

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -55,7 +55,7 @@ export default function Home() {
           place with ease
         </h1>
         <div className='text-gray-400 text-xs sm:text-sm'>
-          Sahand Estate is the best place to find your next perfect place to
+          Angello Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
@@ -73,7 +73,7 @@ export default function Home() {
         {offerListings &&
           offerListings.length > 0 &&
           offerListings.map((listing) => (
-            <SwiperSlide key={listing.id}>
+            <SwiperSlide key={listing._id}>
               <div
                 style={{
                   background: `url(${listing.imageUrls[0]}) center no-repeat`,
